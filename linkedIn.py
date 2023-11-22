7c28f769d58c865a2dbb724fc2e93ae5c2b25db3
Bentalha Aymen <>
2023-11-22 16:04:38 +0100

    Date d'envoie du message sur le CSV, CSV commun avec le bot connection

--- linkedIn.py
+++ linkedIn.py
@@ -15,71 +15,39 @@ short_random = random.randint(5, 20)
 medium_random = random.randint(20, 60)
 long_random = random.randint(60, 100)
 compteur = 0
-mon_message = "Bonjour, contente faire parti de votre réseaux"
+
+def update_or_add_status(file_name, full_name, status):
+    updated_rows = []
+    found = False
+
+    # Lire le fichier CSV existant
+    with open(file_name, mode='r', newline='', encoding='utf-8') as file:
+        reader = csv.reader(file, delimiter=';')
+        for row in reader:
+            # Assurez-vous que chaque ligne a au moins 2 colonnes (nom et date)
+            while len(row) < 2:
+                row.append('')
+
+            # Vérifier si le nom est dans la ligne et mettre à jour ou ajouter le statut
+            if row[0] == full_name:
+                if len(row) == 2:
+                    row.append(status)  # Ajouter le statut si la ligne a seulement 2 colonnes
+                else:
+                    row[2] = status  # Mettre à jour le statut si la 3ème colonne existe déjà
+                found = True
+            updated_rows.append(row)
+
+    # Réécrire le fichier avec les lignes mises à jour
+    with open(file_name, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file, delimiter=';')
+        writer.writerows(updated_rows)
+
+    if found:
+        print(f"OK - Statut '{status}' ajouté pour {full_name}")
+    else:
+        print(f"Pas trouvé: {full_name}")
 
 
-def open_profile(ignore_list=[]):   
-    driver.execute_script("window.open('', '_blank');")
-    driver.switch_to.window(driver.window_handles[-1])
-    driver.get('https://www.linkedin.com/mynetwork/invite-connect/connections/')
-    wait = WebDriverWait(driver, 10)
-    
-    # Localisez la div qui contient les éléments li
-    div_containing_lis = driver.find_element(By.ID, 'ember1375')  # Remplacez 'id_de_la_div' par l'ID réel de la div
-
-    # Trouvez tous les éléments li à l'intérieur de cette div
-    list_items = div_containing_lis.find_elements(By.TAG_NAME, 'li')
-
-    # Créez un objet WebDriverWait pour gérer les attentes
-    wait = WebDriverWait(driver, 10)
-
-# Itérez sur chaque élément li
-    for index, li in enumerate(list_items, start=1):
-        try:
-            # À l'intérieur de chaque li, trouvez le bouton à cliquer
-            # Remplacez 'selector_du_bouton' par le sélecteur approprié pour le bouton
-            button = li.find_elements(By.CSS_SELECTOR, '.scaffold-finite-scroll.scaffold-finite-scroll--finite')
-        
-            # Attendez que le bouton soit cliquable et cliquez dessus
-            wait.until(EC.element_to_be_clickable(button)).click()
-        
-            # Ajoutez ici un délai ou une autre logique si nécessaire, par exemple pour fermer une fenêtre modale qui pourrait apparaître
-        
-        except TimeoutException:
-            print(f"Le bouton dans le li numéro {index} n'est pas devenu cliquable dans le temps imparti.")
-        except ElementClickInterceptedException:
-            print(f"Le clic sur le bouton dans le li numéro {index} a été intercepté par un autre élément.")
-        except Exception as e:
-            print(f"Une erreur s'est produite lors de la tentative de clic sur le bouton dans le li numéro {index}: {e}")
-
-'''
-    message_buttons = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "button[aria-label*='Send a message to']")))
-    mon_message = "Bonjour, contente faire parti de votre réseaux"
-    for button in message_buttons:
-        try:
-            # Scrollez jusqu'au bouton si nécessaire
-            driver.execute_script("arguments[0].scrollIntoView();", button)
-            # Attendez que le bouton soit cliquable et cliquez dessus
-            wait.until(EC.element_to_be_clickable(button)).click()
-            #time.sleep(10)
-            #message_box = wait.until(EC.visibility_of_element_located((By.XPATH, "//div[@role='textbox'][@aria-label='Write a message…']")))
-            #message_box.send_keys(mon_message)
-            time.sleep(10)
-            # Envoyer la touche ESCAPE
-            webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-            #time.sleep(10)
-        except Exception as e:
-            print("Erreur lors du clic sur le bouton de message:", e)
-    html = driver.find_element(By.TAG_NAME, 'html')
-    html.send_keys(Keys.END)
-    time.sleep(5)
-    # Incrémenter la variable globale
-    global compteur   
-    compteur += 1
-    print(compteur)
-    driver.close()  
-    driver.switch_to.window(driver.window_handles[0])  
-'''
 def parse_time(time_string):
     # Cette fonction convertit le texte de temps en un objet datetime
     # Assurez-vous que cette fonction gère tous les formats de temps que LinkedIn peut afficher
@@ -95,7 +63,7 @@ def parse_time(time_string):
 
 
 # Functions
-def search_and_send_request(till_page, writer, ignore_list=[]):
+def search_and_send_request(file_name, till_page, writer, ignore_list=[]):
     for page in range(1):
         print('\nINFO: Checking on page %s' % (page))
         driver.get('https://www.linkedin.com/mynetwork/invite-connect/connections/')
@@ -123,7 +91,7 @@ def search_and_send_request(till_page, writer, ignore_list=[]):
         for connection_time, result in connections_with_time:
 
                 # Vérifier si le temps de connexion est inférieur à 23 heures
-            if connection_time > datetime.now() - timedelta(days=7):
+            if connection_time < datetime.now() - timedelta(days=2):
                 print("%s ) TIME OK: %s" % (index, text))
                     # Trouver le bouton Message
                 message_button = result.find_element(By.CSS_SELECTOR, "button[aria-label*='Send a message to']")
@@ -138,6 +106,7 @@ def search_and_send_request(till_page, writer, ignore_list=[]):
                         name_element = result.find_element(By.CSS_SELECTOR, "span.mn-connection-card__name")
                         full_name = name_element.text
                         first_name = full_name.split(' ')[0] 
+                        print(full_name)
                         time.sleep(10)
 
                         personalized_message = f"Bonjour {first_name}, \nMerci pour la connexion.\n\nAs-tu déjà entendu parler du portage salarial ?"
@@ -149,10 +118,15 @@ def search_and_send_request(till_page, writer, ignore_list=[]):
                         
                         send_button = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.msg-form__send-button")))
                         send_button.click()
+                        print("%s ) MESSAGE SENT: %s" % (index, text))
                         time.sleep(10)
-                        
+                        date_actuelle = datetime.now().strftime("%Y-%m-%d")
+                        update_or_add_status(file_name, full_name, date_actuelle)
+
+
                         webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
                         time.sleep(10)
+
                         print("%s ) MESSAGE SENT: %s" % (index, text))
                     except Exception as e:
                         print('%s ) ERROR: %s' % (index, text))
@@ -172,6 +146,7 @@ try:
     time.sleep(20)
     # CSV file loging
     file_name = parameters.file_name
+    file_name = "/Users/aymen/Documents/linkedin/Bot/connectionsMessages.csv"
     file_exists = os.path.isfile(file_name)
     writer = csv.writer(open(file_name, 'a'))
     if not file_exists: writer.writerow(['Connection Summary'])
@@ -181,8 +156,7 @@ try:
     else:
         ignore_list = []
     # Search
-    search_and_send_request(till_page=parameters.till_page, writer=writer,
-                            ignore_list=ignore_list)
+    search_and_send_request(file_name, till_page=parameters.till_page, writer=writer, ignore_list=ignore_list)
 except KeyboardInterrupt:
     print("\n\nINFO: User Canceled\n")
 except Exception as e:
